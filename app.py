0e220e3f91a94a5efb339f2c4afadb31da8a94bb
Aidan Farhi <aidanmcfarhi@gmail.com>
2022-11-10 05:56:56 -0500

    added print statements

--- app.py
+++ app.py
@@ -4,11 +4,13 @@ app = Flask(__name__)
 
 @app.route('/', methods=['GET'])
 def index():
+    print({'status': 'alive'})
     return {'status': 'alive'}
 
 @app.route('/echo', methods=['POST'])
 def echo():
     data = request.get_json(force=True)
+    print(data)
     return {'received': data}
 
 if __name__ == '__main__':
